61e478c7b3f1c006b9aa3c5a3c7e7528b7f9d436
jambottle <jaewon.walks@gmail.com>
2021-10-25 18:00:43 +0900

    Add: Routing.js 생성 및 폴더/ 파일 구성

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+import Feed from './pages/Feed/Feed';
+import Jobs from './pages/Jobs/Jobs';
+import Profile from './pages/Jobs/Jobs';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/feed" component={Feed} />
+        <Route exact path="/jobs" component={Jobs} />
+        <Route exact path="/profile" component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ src/pages/SignIn/SignIn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SignIn() {
+  return <div>페이지 초기화</div>;
+}
+
+export default SignIn;
