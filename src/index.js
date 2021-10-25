640745fb4cb819c3b59f94adbee2f5141dc13647
jambottle <jaewon.walks@gmail.com>
2021-10-25 18:31:38 +0900

    Modify: GlobalStyle 적용 위치를 index.js 안으로 변경

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-import GlobalStyle from './styles/GlobalStyle';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+	// 전역스타일
+
+`;
 
 ReactDOM.render(
   <React.StrictMode>
