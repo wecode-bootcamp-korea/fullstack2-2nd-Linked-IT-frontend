116b803ae9fe9c07f87963edb00a6b8f8f2d2935
siwon <ksw3721@gmail.com>
2021-10-27 03:14:56 +0900

    ADD: UserCard 컴포넌트구현 & review 반영

---
+++ src/components/UserCard/testDate.js
@@ -0,0 +1,22 @@
+const profile = {
+  id: '1',
+  firstName: '시원',
+  lastName: '김',
+  userProfileUrl:
+    'http://img1.tmon.kr/cdn3/deals/2021/01/08/5098632134/5098632134_front_f7e0113dd3.jpg',
+  // 회사 관련 정보
+  companyNameEng: 'k-cool, Inc.',
+  companyNameKor: '(주)케이쿨',
+  currentPosition: 'CEO',
+  // 개인 부가 정보
+  country: 'South Korea',
+  city: 'Seoul',
+  // 교육관련 정보
+  school: 'Chicken University',
+  major: 'Business',
+  degree: 'Bachelor of Arts(BA)',
+  // 친구관꼐
+  isConnection: false,
+};
+
+export default profile;
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,8 +1,19 @@
-import React from 'react';
+// import React from 'react';
 import styled from 'styled-components';
+import UserCard from '../../components/UserCard/UserCard';
+import profileData from '../../components/UserCard/testDate';
 
 function Profile() {
-  return <div>페이지 초기화</div>;
+  return (
+    <div>
+      <UserCard
+        profile={profileData}
+        relation="true"
+        type="ejob-l education-l location"
+        text={`${profileData.firstName}님에게 메시지를 보내보세요!`}
+      />
+    </div>
+  );
 }
 
 export default Profile;
