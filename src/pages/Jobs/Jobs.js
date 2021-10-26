269189812ddb30501392c1e848e799e45418ec68
Brad Han <imgum9@gmail.com>
2021-10-26 15:18:46 +0900

    Fix: conflict

--- src/pages/Jobs/Jobs.js
+++ src/pages/Jobs/Jobs.js
@@ -1,8 +1,31 @@
-import React from 'react';
 import styled from 'styled-components';
+import JobPosting from './JobPosting';
+import jobsMockData from './data/JobsMockData';
 
-function Jobs() {
-  return <div>페이지 초기화</div>;
+export default function Jobs() {
+  return (
+    <StyledJobs>
+      <JobLPostingList>
+        {jobsMockData.map((post, idx) => {
+          return <JobPosting key={idx} {...post} />;
+        })}
+      </JobLPostingList>
+      <JobLPostingDetail></JobLPostingDetail>
+    </StyledJobs>
+  );
 }
 
-export default Jobs;
+const StyledJobs = styled.div`
+  display: flex;
+  width: 1128px;
+  margin: 0 auto;
+`;
+
+const JobLPostingList = styled.section`
+  width: 40%;
+`;
+
+const JobLPostingDetail = styled.section`
+  width: 60%;
+  border: 1px solid #ebebeb;
+`;
