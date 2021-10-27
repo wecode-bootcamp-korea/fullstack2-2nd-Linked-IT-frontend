9e7510a1c5c97d72e31f3763121a44a2b7172017
Brad Han <imgum9@gmail.com>
2021-10-27 18:00:51 +0900

    Add: JobPostingCard component

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,13 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-    ${reset}
-    * {
-        box-sizing: border-box;
-    }
-    a {
-        text-decoration: none;
-    }
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+  }
+  a:visited {
+    color: black;
+  }
 `;
 
 export default GlobalStyle;
