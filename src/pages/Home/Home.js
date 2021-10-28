18829b822cf54ddb2594d228bad8e38ddf27a24b
jambottle <jaewon.walks@gmail.com>
2021-10-29 01:50:14 +0900

    Modify: useState 함수와 onClick 이벤트를 활용한 Footer 컴포넌트의 Popup 효과 수정

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Footer from '../../components/Footer/Footer';
 
 export default function Home() {
+  const [isFooterDefault, setIsFooterDefault] = useState(false);
+  const [isFooterHidden, setIsFooterHidden] = useState(false);
+
+  const toggleFooter = () => {
+    setIsFooterHidden(!isFooterHidden);
+  };
+
   return (
     <>
-      <div>페이지 초기화</div>
-      <Footer popup={true} />
+      {!isFooterDefault && isFooterHidden ? (
+        <button onClick={() => toggleFooter()}>더 보기</button>
+      ) : (
+        <button>더 보기</button>
+      )}
+      <Footer
+        isDefault={isFooterDefault}
+        isHidden={isFooterHidden}
+        toggleFooter={toggleFooter}
+      />
     </>
   );
 }
