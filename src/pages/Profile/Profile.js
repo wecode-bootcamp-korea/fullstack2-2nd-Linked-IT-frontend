b96dd253adcc53227f42276cd6ac945d756136ac
siwon <ksw3721@gmail.com>
2021-10-28 18:59:17 +0900

    ADD: Profile 페이지 캐러셀 구현

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,15 +1,15 @@
-import styled from 'styled-components';
-import UserCard from '../../components/UserCard/UserCard';
-import profileData from '../../components/UserCard/testData';
+import Carousel from './Carousel/Carousel';
+
+import cards from './Carousel/CardMockData';
 
 function Profile() {
   return (
     <div>
-      <UserCard
-        profile={profileData}
-        relation="true"
-        type="ejob-l education-l location"
-        text={`${profileData.firstName}님에게 메시지를 보내보세요!`}
+      <Carousel
+        cards={cards}
+        cardWidth={350}
+        cardMargin={10}
+        wrapperWidth={700}
       />
     </div>
   );
