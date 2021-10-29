e31deec2acd729c638051f862698b532932fcb91
jambottle <jaewon.walks@gmail.com>
2021-10-29 17:45:02 +0900

    Add: PageHeader 컴포넌트 완성

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function SignIn() {
-  return <div>페이지 초기화</div>;
+  return (
+    <>
+      <SignInPageHeader>
+        <Link to="/">
+          <img alt="LinkedIT Logo" src="/images/common_logo_full.png" />
+        </Link>
+      </SignInPageHeader>
+    </>
+  );
 }
+
+const SignInPageHeader = styled.header`
+  position: absolute;
+  top: 0;
+
+  width: 100%;
+
+  a {
+    display: block;
+    margin: 2em 0 0 3.5em;
+
+    img {
+      width: 7rem;
+    }
+  }
+`;
