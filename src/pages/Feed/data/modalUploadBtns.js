452f3e4244549ab4d10e47081c31b8c547d02a6b
JivenHan <87740944+JivenHan@users.noreply.github.com>
2021-11-12 14:33:37 +0900

    Images->images 못바꾼거 수정 (#90)
    * Image->image로 수정

--- src/pages/Feed/data/modalUploadBtns.js
+++ src/pages/Feed/data/modalUploadBtns.js
@@ -1,11 +1,11 @@
 const modalUploadBtns = [
-  { alt: 'img', src: '/Images/ico_image.svg' },
-  { alt: 'video', src: '/Images/ico_video.svg' },
-  { alt: 'docu', src: '/Images/ico_document.svg' },
-  { alt: 'case', src: '/Images/ico_briefcase.svg' },
-  { alt: 'badge', src: '/Images/ico_badge.svg' },
-  { alt: 'bars', src: '/Images/ico_bars.svg' },
-  { alt: 'etc', src: '/Images/ico_etc.svg' },
+  { alt: 'img', src: '/images/ico_image.svg' },
+  { alt: 'video', src: '/images/ico_video.svg' },
+  { alt: 'docu', src: '/images/ico_document.svg' },
+  { alt: 'case', src: '/images/ico_briefcase.svg' },
+  { alt: 'badge', src: '/images/ico_badge.svg' },
+  { alt: 'bars', src: '/images/ico_bars.svg' },
+  { alt: 'etc', src: '/images/ico_etc.svg' },
 ];
 
 export default modalUploadBtns;
